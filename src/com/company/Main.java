917afbe186a21d194c113edcc2247aa23117cf0d
gashig007 <rustamgashig@gmail.com>
2021-12-10 15:31:31 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,116 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefence = "";
+
+    public static int[] heroesHealth = {270, 260, 250, 300};
+    public static int[] heroesDamage = {15, 20, 25, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic"};
+    public static int round_number = 0;
+
+    public static void main(String[] args) {
+        printStatistics(); // До начала игры вывод статистики
+        while (!isGameFinished()) {
+            round();
+        }
+    }
+
+    public static void changeBossDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length-1); //0,1,2
+        bossDefence = heroesAttackType[randomIndex];
+        System.out.println("Boss chose " + bossDefence);
+    }
+
+    public static void round() {
+        round_number++;
+        changeBossDefence();
+        bossHits();
+        heroesHit();
+        printStatistics();
+    }
+
+    public static void heroesHit() {
+        Random ramdom1 = new Random();
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (heroesAttackType[i] == bossDefence) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(9) + 2; //2,3,4,5,6,7,8,9,10
+                    if (bossHealth - heroesDamage[i] * coeff < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i] * coeff;
+                    }
+                    System.out.println("Critical damage " + heroesDamage[i] * coeff);
+                } else {
+                    if (bossHealth - heroesDamage[i] < 0) {
+                        bossHealth = 0;
+                    } else {
+                        bossHealth = bossHealth - heroesDamage[i];
+                    }
+                    if(heroesHealth[i] < 100){
+                        heroesHealth[i] = ramdom1.nextInt(20) + heroesHealth[i];
+                    }
+                    else if (heroesHealth[3] < 100){
+                        heroesHealth[3] = heroesHealth[3];
+                    }
+
+                }
+            }
+        }
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                }
+                else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+
+            }
+        }
+    }
+
+    public static boolean isGameFinished() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0) {
+            System.out.println("Boss won!!!");
+            return true;
+        }
+        return false;*/
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+        }
+        return allHeroesDead;
+    }
+
+    public static void printStatistics() {
+        System.out.println(round_number + " ROUND _______________");
+        System.out.println("Boss health: " + bossHealth + " (" + bossDamage + ")");
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: "
+                    + heroesHealth[i] + " (" + heroesDamage[i] + ")");
+        }
+        System.out.println("_______________");
+    }
+}
+
